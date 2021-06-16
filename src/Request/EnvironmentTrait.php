ec0d3b1429d42ba862d80762032156d997a21e7a
pfaciana <phil@renderdev.com>
2021-06-16 01:44:26 -0400

    Keep track of when tor was last restarted across all instances of the EnvironmentTrait

--- src/Request/EnvironmentTrait.php
+++ src/Request/EnvironmentTrait.php
@@ -6,6 +6,8 @@ namespace RequestClient\Request;
 
 trait EnvironmentTrait
 {
+	protected $torRestartedAt = NULL;
+
 	public function isWindows ()
 	{
 		return strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
@@ -18,12 +20,31 @@ trait EnvironmentTrait
 		return !empty(exec($command));
 	}
 
-	public function restartTor ()
+	public function restartTor ($sleep = 3)
 	{
+		global $torRestartedAt;
+
 		$output = shell_exec(($this->isWindows() ? 'wsl ' : '') . 'sudo service tor restart');
-		sleep(3);
+		sleep($sleep);
+
+		$torRestartedAt = $this->torRestartedAt = time();
 
 		return $output;
+	}
+
+	public function hasTorBeenRestarted ($sync = TRUE)
+	{
+		global $torRestartedAt;
+
+		if ($torRestartedAt === $this->torRestartedAt) {
+			return FALSE;
+		}
+
+		if (!empty($sync)) {
+			$this->torRestartedAt = $torRestartedAt;
+		}
+
+		return TRUE;
 	}
 
 	public function getIP ($skipTor = FALSE)
@@ -31,6 +52,6 @@ trait EnvironmentTrait
 		$torify = !$skipTor && $this->isTorEnabled() ? 'torify' : '';
 		$wsl    = $this->isWindows() ? 'wsl' : '';
 
-		return exec("{$wsl} {$torify} curl ifconfig.me 2>&1");
+		return trim(exec(trim("{$wsl} {$torify} curl ifconfig.me 2>&1")));
 	}
 }
