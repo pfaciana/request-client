2290c1729008920f4227189c31c994131dfdd925
Phil Faciana <phil@renderdev.com>
2021-10-27 08:14:44 -0400

    Added notes about class and trait structures

--- src/Curl.php
+++ src/Curl.php
@@ -2,6 +2,23 @@
 
 namespace RequestClient;
 
+/*
+ * Class and Trait structures
+ *
+ * Curl
+ *    extends CurlSession
+ *        includes CurlBrowserOptions (as $this->options)
+ *            use CurlProxyTrait
+ *                use EnvironmentTrait
+ *        extends CurlBrowser
+ *            use CurlOptionsTrait
+ *            extends Browser
+ *                use UserAgentTrait
+ *                use QueryTrait
+ *                use EnvironmentTrait (same as CurlBrowserOptions usage, but it exposes this to the user instance)
+ *
+ */
+
 class Curl extends CurlSession
 {
 	public function request ($url, $options = [])
